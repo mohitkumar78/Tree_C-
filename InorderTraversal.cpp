d12f2c7215c668fd4afac5e5ee55f30df730e0ad
mohitkumar78 <129609791+mohitkumar78@users.noreply.github.com>
2024-09-10 01:10:29 +0530

    inorderTravel

---
+++ InorderTraversal.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+  Node* right;
+  Node* left;
+  int data;
+
+  Node(int val){
+    data = val;
+    right=left=nullptr;
+  }
+};
+
+Node* insert(Node* root,int value){
+
+    if(root == nullptr){
+        return new Node(value);
+    }
+    if( value < root->data){
+        root->left = insert(root->left,value);
+    }
+    else if(value > root->data){
+        root->right = insert(root->right,value);
+    }
+    return root;
+}
+void InOrderTraversal(Node* root){
+    if(root == nullptr){
+        return;
+    }
+    InOrderTraversal(root->left);
+    cout<<root->data<<"   ";
+    InOrderTraversal(root->right);
+}
+int main(){
+    Node* root = nullptr;
+    root = insert(root,50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
+    cout<<"bst is created";
+    cout<<"inoredr traversal of tree";
+    InOrderTraversal(root);
+    return 0;
+}
